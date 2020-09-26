975a3fff404d274f778b3d72826002b13b7765d9
alex-suschin <alex_suschin@mail.ru>
2020-09-26 15:08:41 +0300

    pre-slider

--- src/js/index.js
+++ src/js/index.js
@@ -31,57 +31,16 @@ $(function() {
     });
 
 
-    // $('.b-mobile_open_nav').click(function() {
-    //     $(this).toggleClass('b-mobile_open_nav_active');
-    //     if ($(this).hasClass('b-mobile_open_nav_active')) {
-    //         $('.b-links').addClass('b-links_active');
-    //         $('.b-links a').each(function(index, el) {
-    //             setTimeout(function() {
-    //                 $(el).addClass('showed');
-    //             }, index * 100);
-    //         });
-    //     } else {
-    //         var time = 0;
-    //         $($('.b-links a').get().reverse()).each(function(index, el) {
-    //             time = index * 100;
-    //             setTimeout(function() {
-    //                 $(el).removeClass('showed');
-    //             }, time);
-    //         });
-    //         setTimeout(function() {
-    //             $('.b-links').removeClass('b-links_active');
-    //             $('.b-links').find('.showed').removeClass('showed');
-    //         }, $('.b-links a').length * 100);
-    //     }
-    // });
-
-
-    // $('input[type=tel]').bind("change keyup input click", function() {
-    //     if (this.value.match(/[^\+0-9]/g)) {
-    //         this.value = this.value.replace(/[^\+0-9]/g, '');
-    //     }
-    // });
-
-    // $('input[type=tel]').on('focus', function() {
-    //     if ($(this).val() == '') $(this).val('+7');
-    // });
-
-    // $('input[type=tel]').on('blur', function() {
-    //     if ($(this).val() == '+7') $(this).val('');
-    // });
-
-    // $('.style-tab').click(function() {
-    //     $('.style-tabs').find('.active').removeClass('active');
-    //     $(this).addClass('active');
-    //     $('.styles-elems').find('.styles-elem-item').hide();
-    //     $('#' + $(this).data('switch')).show();
-    // });
-
-    $('.programms-btns a').click(function() {
-        $('.programms-btns').find('.active').removeClass('active');
+    $('.price-btns a').click(function() {
+        $('.price-btns').find('.active').removeClass('active');
         $(this).addClass('active');
-        $('.programms-box').find('.programms-elem').hide();
+        $('.price-tables').find('.table-price').hide();
         $('#' + $(this).data('switch')).show();
+    });
+
+    $('.btn-more').click(function() {
+        $('.table-price-row').css('display', 'table-row');
+        $(this).hide();
     });
 
     $(window).on('load resize scroll', function() {
@@ -93,11 +52,5 @@ $(function() {
         }
 
     });
-
-
-
-    //   $(document).on('af_complete', function(event, response) {
-    //     if(response.success) $.fancybox.close();
-    // })
 
 });
